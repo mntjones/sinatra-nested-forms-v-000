29b2561491d0fc6571b1a7d28b613da15621dbd8
Monica Tamaru Jones <mntamaru@gmail.com>
2018-05-15 18:58:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require './environment'
+<<<<<<< HEAD
 require_relative './app/models/pirate.rb'
 require_relative './app/models/ship.rb'
+=======
+>>>>>>> 6bf1e72dd78da92f91018b98098431b5dcebfc50
 require 'pry'
 
 module FormsLab
@@ -12,14 +15,29 @@ module FormsLab
     end
     
     get '/new' do
+<<<<<<< HEAD
       erb :'pirates/new'
+=======
+      erb :"pirates/new"
+>>>>>>> 6bf1e72dd78da92f91018b98098431b5dcebfc50
     end
     
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
+<<<<<<< HEAD
 
       @ships = Ship.all
       erb :'pirates/show'
+=======
+ 
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      
+      @ships = Ship.all
+      binding.pry
+      erb :"pirates/show"
+>>>>>>> 6bf1e72dd78da92f91018b98098431b5dcebfc50
     end
     
   end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -8,10 +8,13 @@ class Pirate
     @weight = params[:weight]
     @height = params[:height]
     @@pirates << self
+<<<<<<< HEAD
     
     params[:ships].each do |details|
         Ship.new(details)
     end
+=======
+>>>>>>> 6bf1e72dd78da92f91018b98098431b5dcebfc50
   end
   
   def self.all
